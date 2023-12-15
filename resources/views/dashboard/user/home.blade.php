d054c198e4c4d9a6bbe859e2ab7f1e6eb1c2d2ab
Meziane Maryem <72824836+MezianeMaryem@users.noreply.github.com>
2023-12-15 20:37:01 +0100

    Update home.blade.php

--- resources/views/dashboard/user/home.blade.php
+++ resources/views/dashboard/user/home.blade.php
@@ -239,7 +239,11 @@ button[type='submit'] {
             
  @foreach ($localDocuments as $document)
         <tr>
-            <td>{{ $document->nom }}</td>
+            <td>  @php
+        $parts = explode('_', $document->nom, 2); // Sépare le nom en utilisant '_' comme séparateur
+        $displayName = count($parts) > 1 ? $parts[1] : $document->nom; // Utilise la partie après '_', sinon le nom entier
+    @endphp
+    {{ $displayName }}</td>
             <td>{{ $document->nom_utilisateur }}</td>
             <td>{{ $document->Univ }}</td>
             <td>{{ $document->created_at }}</td>
@@ -249,7 +253,11 @@ button[type='submit'] {
     @endforeach
     @foreach ($publicDocuments as $document)
         <tr>
-            <td>{{ $document->nom }}</td>
+            <td>  @php
+        $parts = explode('_', $document->nom, 2); // Sépare le nom en utilisant '_' comme séparateur
+        $displayName = count($parts) > 1 ? $parts[1] : $document->nom; // Utilise la partie après '_', sinon le nom entier
+    @endphp
+    {{ $displayName }}</td>
             <td>{{ $document->nom_utilisateur }}</td>
             <td>{{ $document->Univ }}</td>
             <td>{{ $document->created_at }}</td>
